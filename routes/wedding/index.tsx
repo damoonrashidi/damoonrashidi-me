4befacc6fdf7387a9a7ae72079dd869adf9efc8d
Damoon Rashidi <damoon.rashidi@gmail.com>
2024-07-07 14:32:08 +0200

    Update wedding page

--- routes/wedding/index.tsx
+++ routes/wedding/index.tsx
@@ -1,3 +1,46 @@
 export default function Brollop() {
-  return <div>Brollop</div>;
+  return (
+    <div className="m-auto max-w-prose px-8 py-8 font-display">
+      <h1 className="font-display text-center">Anna & Damoon</h1>
+      <p className="text-center">21 September</p>
+
+      <div className="my-8">
+        <h3 className="pt-4">
+          <span className="inline-block w-[90px]">15:00</span> Vigselceremoni
+        </h3>
+        <p>
+          <a
+            href="https://maps.app.goo.gl/dCvjE6eX2vw8kYXz7"
+            target="_blank"
+            rel="noopener nofollow"
+          >
+            Brännkyrka Kyrka
+          </a>
+          , Götalandsvägen 189, 193, 125 35 Älvsjö
+        </p>
+
+        <h3 className="pt-4">
+          <span className="inline-block w-[90px]">16:00</span> Bröllopsfest hos
+          Annas föräldrar
+        </h3>
+        <p>
+          <a
+            href="https://maps.app.goo.gl/iQCi9C5keydvpaQEA"
+            target="_blank"
+            rel="noopener nofollow"
+          >
+            Mölnbovägen 17, 124 32, Bandhagen
+          </a>
+        </p>
+      </div>
+
+      <h3 className="pt-4">
+        <span className="inline-block w-[90px]">17:30</span> Mat
+      </h3>
+
+      <h3 className="pt-4">
+        <span className="inline-block w-[90px]">Kväll</span> Grill
+      </h3>
+    </div>
+  );
 }
