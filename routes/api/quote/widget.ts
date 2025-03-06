cb215a8eea50a2899aae35a165df568fa696aac4
Damoon Rashidi <damoon.rashidi@plock.io>
2025-03-06 18:30:24 +0100

    widget response

--- routes/api/quote/widget.ts
+++ routes/api/quote/widget.ts
@@ -1,12 +1,41 @@
 import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = () => {
-  return Response.json({
-    type: "IFRAME",
-    width: 890,
-    height: 748,
-    uri: "https://damoonrashidi.me/iframe",
-    label: "Edit",
-    associatedObjectProperties: [],
-  });
+  const response = {
+    results: [
+      {
+        objectId: 245,
+        title: "API-22: APIs working too fast",
+        link: "http://example.com/1",
+        created: "2016-09-15",
+        priority: "HIGH",
+        project: "API",
+        description:
+          "Customer reported that the APIs are just running too fast. This is causing a problem in that they're so happy.",
+        reporter_type: "Account Manager",
+        status: "In Progress",
+        ticket_type: "Bug",
+        updated: "2016-09-28",
+        actions: [
+          {
+            type: "IFRAME",
+            width: 890,
+            height: 748,
+            uri: "https://damoonrashidi.me/iframe",
+            label: "Edit",
+            associatedObjectProperties: [],
+          },
+        ],
+      },
+    ],
+    primaryAction: {
+      type: "IFRAME",
+      width: 890,
+      height: 748,
+      uri: "https://damoonrashidi.me/iframe",
+      label: "Settings",
+    },
+  };
+
+  return Response.json(response);
 };
